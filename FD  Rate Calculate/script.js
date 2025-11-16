e2e0ba2019ac468efbd1d21d47f57c8ee9042da8
Yuvan12128 <yuvan12128@.com>
2025-11-16 08:32:06 +0530

    add fd rating calculation

---
+++ FD  Rate Calculate/script.js
@@ -0,0 +1,13 @@
+let input=Number(document.getElementById('input').value)
+let result=document.getElementById('result')
+function button(){
+    let rate=0;
+    if(input<3){
+        rate=5.8
+    }else if(input >=7 && input <=9){
+        rate=6.5
+    }else {
+        rate=7
+    }
+    result.innerHTML='your interest rate is '+rate+' %'
+}
