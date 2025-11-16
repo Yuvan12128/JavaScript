434d8c55ccb5c2bf6d4f5f9977eeaef2f5248e11
Yuvan12128 <yuvan12128@.com>
2025-11-16 07:56:19 +0530

    submit type a sentence project

---
+++ Type a sentence/script.js
@@ -0,0 +1,5 @@
+function calculate(){
+    let input=document.getElementById('input').value
+    let result=document.getElementById('result')
+    result.innerHTML=input.length+' Total Length of Sentence'
+}
